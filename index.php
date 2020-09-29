6a57dee38d6afd04446ab1e65b452ac155436bc4
Pushkarevskiy <admin@PushMac.local>
2020-09-30 01:28:40 +0300

    debug

--- index.php
+++ index.php
@@ -819,60 +819,40 @@ function GetShowCases()
     $case_id = '';
     $condition = '';
 
-    // if (isset($_GET['id']) && $_GET['id'] != '') {
-    //     $condition = 'where us.id=' . intval($_GET['id']);
-    // } else if (isset($_GET['user_id']) && $_GET['user_id'] != '') {
-    //     $condition = 'where us.user_id=' . intval($_GET['user_id']) . ' order by adding_time desc';
-    // } else if (isset($_GET['favorites']) && $_GET['favorites']) {
-    //     $sql_get_favorite_case_id = $db->Execute('select case_id from `case_favorite` where user_id=' . SqlGetUserId());
-    //     $case_ids = [];
-    //     if ($sql_get_favorite_case_id) {
-    //         while (!$sql_get_favorite_case_id->EOF) {
-    //             $case_ids[] = $sql_get_favorite_case_id->Fields('case_id');
-    //             $sql_get_favorite_case_id->MoveNext();
-    //         }
-    //         $condition = 'where us.id in (' . implode(',', $case_ids) . ') order by adding_time desc';
-    //     } else {
-    //         result_text(1, []);
-    //         return;
-    //     }
-    // } else {
-    //     $condition = 'order by adding_time desc';
-    // }
-
-    $sql_get_show_cases = $db->Execute('select '
-        . 'us.id, '
-        . 'us.user_id, '
-        . 'us.adding_time, '
-        . 'us.update_time, '
-        . 'us.case_name, '
-        . 'us.photo_url, '
-        . 'us.price, '
-        . 'us.old_price, '
-        . 'us.type, '
-        . 'us.full_type, '
-        . 'us.detail_type, '
-        . 'us.state, '
-        . 'us.wheel_size, '
-        . 'us.velo_type, '
-        . 'us.direction, '
-        . 'us.description, '
-        . 'us.additionalPhotos, '
-        . 'us.active, '
-        . 'uc.name, '
-        . 'uc.photo, '
-        . 'uc.surname, '
-        . 'uc.area, '
-        . 'uc.telegram, '
-        . 'uc.phone, '
-        . 'uc.phone2, '
-        . 'uc.vk, '
-        . 'uc.facebook, '
-        . 'uc.instagram '
-        . 'from `user_showcase` us '
-        . 'inner join `user_contacts` uc '
-        . 'on us.user_id=uc.user_id '
-        . 'order by adding_time desc');
+    $sql_get_show_cases = $db->Execute('select
+    us.id,
+    us.user_id,
+    us.adding_time,
+    us.update_time,
+    us.case_name,
+    us.photo_url,
+    us.price,
+    us.old_price,
+    us.type,
+    us.full_type,
+    us.detail_type,
+    us.state,
+    us.wheel_size,
+    us.velo_type,
+    us.direction,
+    us.description,
+    us.additionalPhotos,
+    us.active,
+    uc.name,
+    uc.photo,
+    uc.surname,
+    uc.area,
+    uc.telegram,
+    uc.phone,
+    uc.phone2,
+    uc.vk,
+    uc.facebook,
+    uc.instagram
+    from `user_showcase` us
+    inner join `user_contacts` uc
+    on us.user_id=uc.user_id
+    order by adding_time desc');
+        
         result_text(0, $sql_get_show_cases);
 
     if ($sql_get_show_cases) {
