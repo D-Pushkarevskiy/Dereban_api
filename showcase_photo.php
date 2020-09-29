21a0899f2c38696be468dfb841142db20ac6582e
Pushkarevskiy <admin@PushMac.local>
2020-09-29 17:50:14 +0300

    debug

--- showcase_photo.php
+++ showcase_photo.php
@@ -20,7 +20,7 @@ if (is_uploaded_file($photo)) {
     if (move_uploaded_file($photo, $destination . $user_photo_path)) {
         result_text(0, $file_path . $user_photo_path);
     } else {
-        result_text(1, 'NO_HAVE_PERMISSION_DIRECTORY');
+        result_text(1, 'NO_HAVE_PERMISSION_DIRECTORY = ' . $destination);
     }
 } else {
     result_text(1, 'FAILED_TO_UPLOAD_FILE');
