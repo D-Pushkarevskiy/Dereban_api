c362426441433a908b8a6ae69746a0ab10944c8c
Pushkarevskiy <admin@PushMac.local>
2020-09-29 17:46:04 +0300

    changes in photos paths

--- showcase_photo.php
+++ showcase_photo.php
@@ -8,15 +8,12 @@ $user_photo_path = 'date' . $date . '-user_id-' . SqlGetUserId() . '.jpg';
 $photo = $_FILES['photo']['tmp_name'];
 
 if (is_uploaded_file($photo)) {
-    if (!file_exists(ADS_IMAGES_PATH)) {
-        mkdir(ADS_IMAGES_PATH);
-    }
     if (!file_exists($destination)) {
         mkdir($destination);
-    }
-    $destination .= '/' . $file_path;
-    if (!file_exists($destination)) {
-        mkdir($destination);
+        $destination .= '/' . $file_path;
+        if (!file_exists($destination)) {
+            mkdir($destination);
+        }
     }
 
     //Перемещаем файл из временной папки в указанную
