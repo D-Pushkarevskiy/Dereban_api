85fa291cb9d8e39fa12ed2b7b7a5f500c3f0e2b5
Pushkarevskiy <admin@PushMac.local>
2020-09-30 01:36:13 +0300

    debug

--- index.php
+++ index.php
@@ -819,41 +819,9 @@ function GetShowCases()
     $case_id = '';
     $condition = '';
 
-    $sql_get_show_cases = $db->Execute('select
-    us.id,
-    us.user_id,
-    us.adding_time,
-    us.update_time,
-    us.case_name,
-    us.photo_url,
-    us.price,
-    us.old_price,
-    us.type,
-    us.full_type,
-    us.detail_type,
-    us.state,
-    us.wheel_size,
-    us.velo_type,
-    us.direction,
-    us.description,
-    us.additionalPhotos,
-    us.active,
-    uc.name,
-    uc.photo,
-    uc.surname,
-    uc.area,
-    uc.telegram,
-    uc.phone,
-    uc.phone2,
-    uc.vk,
-    uc.facebook,
-    uc.instagram
-    from `user_showcase` us
-    inner join `user_contacts` uc
-    on us.user_id=uc.user_id
-    order by adding_time desc');
-        
-        result_text(0, $sql_get_show_cases->Fields('id'));
+    $sql_get_show_cases = $db->Execute('select us.id, us.user_id, us.adding_time, us.update_time, us.case_name, us.photo_url, us.price, us.old_price, us.type, us.full_type, us.detail_type, us.state, us.wheel_size, us.velo_type, us.direction, us.description, us.additionalPhotos, us.active, uc.name, uc.photo, uc.surname, uc.area, uc.telegram, uc.phone, uc.phone2, uc.vk, uc.facebook, uc.instagram from `user_showcase` us inner join `user_contacts` uc on us.user_id=uc.user_id order by adding_time desc');
+    
+    result_text(1, $sql_get_show_cases);
         return;
 
     if ($sql_get_show_cases) {
